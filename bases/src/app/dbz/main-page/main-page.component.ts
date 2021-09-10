52aacd93e603db970a24c2dcee41b46c6aec1ca3
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-10 11:31:10 +0200

    58-ngmodel (two way databinding)

--- bases/src/app/dbz/main-page/main-page.component.ts
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
+
 export class MainPageComponent {
 
   constructor() { }
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000
+  }
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
+  }
+
   agregar(event: any) {
     event.preventDefault();
-    console.log("TEST")
+    console.log(this.nuevo)
   }
 
 }
