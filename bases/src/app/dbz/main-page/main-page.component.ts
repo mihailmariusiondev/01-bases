a0545a189aaf3ac40ae926ce5dcbeab61484384b
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-10 11:51:46 +0200

    59-mostrar-listado-personajes

--- bases/src/app/dbz/main-page/main-page.component.ts
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -13,19 +13,40 @@ interface Personaje {
 export class MainPageComponent {
 
   constructor() { }
+  
+  personajes: Personaje[] = [
+    {
+      nombre: "Goku",
+      poder: 15000
+    },
+    {
+      nombre: "Vegeta",
+      poder: 7500
+    },    
+  ]
 
   nuevo: Personaje = {
-    nombre: 'Trucks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   cambiarNombre(event: any) {
     console.log(event.target.value);
   }
 
-  agregar(event: any) {
-    event.preventDefault();
+  agregar() {
+    if(this.nuevo.nombre.trim().length === 0 || this.nuevo.poder === 0) {
+      return
+    };
+    
+    // event.preventDefault();
     console.log(this.nuevo)
+
+    this.personajes.push(this.nuevo)
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
 
 }
