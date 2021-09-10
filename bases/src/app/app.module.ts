326b371d6f2940eef97b39dfda37d69e2162b1c2
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-10 14:32:03 +0200

    62-tarea-inputs

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// import { ContadorComponent } from './contador/contador/contador.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
 import { DbzModule } from './dbz/dbz.module';
--- bases/src/app/dbz/main-page/main-page.component.ts
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -8,8 +8,6 @@ import { Personaje } from '../interfaces/dbz.interface';
 
 export class MainPageComponent {
 
-  constructor() { }
-
   personajes: Personaje[] = [
     {
       nombre: "Goku",
@@ -22,27 +20,8 @@ export class MainPageComponent {
   ]
 
   nuevo: Personaje = {
-    nombre: '',
+    nombre: "",
     poder: 0
-  }
-
-  cambiarNombre(event: any) {
-    console.log(event.target.value);
-  }
-
-  agregar() {
-    if (this.nuevo.nombre.trim().length === 0 || this.nuevo.poder === 0) {
-      return
-    };
-
-    // event.preventDefault();
-    console.log(this.nuevo)
-
-    this.personajes.push(this.nuevo)
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
   }
 
 }
